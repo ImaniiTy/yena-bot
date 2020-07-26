91ee5fa6cccb221be5f53f05eccb5cd315dfed52
CrazyMega <8453726+CrazyMega@users.noreply.github.com>
2020-07-26 02:08:30 -0300

    da comit crazy...

--- bot.js
+++ bot.js
@@ -2,16 +2,16 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection()
 
-const commands = require('./commands')
-
-
 const prefix = "..";
 
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
-client.once('ready', () => {
-	console.log('Ready!');
-});
+const commandFiles = require('./commands');
+
+for (const file of commandFiles) {
+
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    client.commands.set(command.name, command);
+}
 
 client.on('message', message => {
     // ignore messages without the prefix
@@ -20,18 +20,8 @@ client.on('message', message => {
     const args = message.content.slice(prefix.length).trim().split(/ +/)
     const command = args.shift().toLowerCase();
 
-    for (const file of commandFiles) {
-        const command = require(`./commands/${file}`);
-
-        // set a new item in the Collection
-        // with the key as the command name and the value as the exported module
-        client.commands.set(command.name, command);
-    }
-
-    message.channel.send(command);
-    if(message.content === `${prefix}ping`) {
-        message.channel.send('Pong.')
-        message.channel.send(`${client.emojis.cache.get('662838179438788619')}`)
+    if (command === 'ping') {
+        client.commands.get('ping').execute(message, args)
     }
 })
 
