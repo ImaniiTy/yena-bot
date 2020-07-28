bc4c8358c427a4e1269ca79524512fa7f5022f96
ImaniiTy <augusto.vieira@dcomp.ufs.br>
2020-07-28 05:26:03 -0300

    Feat: radio kpop/jpop

--- modules/music_module.js
+++ modules/music_module.js
@@ -1,10 +1,15 @@
-const { Message } = require("discord.js");
+const { Message, VoiceChannel } = require("discord.js");
 const ytdl = require("ytdl-core-discord");
+const mm = require("music-metadata");
+const axios = require("axios").default;
 
 class MusicModule {
     constructor() {
         this.playlist = [];
         this.isPlaying = false;
+        this.isLoading = false;
+        this.messageChannel;
+        this.radioInterval;
         this.connection;
         this.currentStream;
         this.currentMusic;
@@ -16,8 +21,6 @@ class MusicModule {
      * @param {*} args
      */
     async play(message, args) {
-        const channel = message.member.voice.channel;
-
         if (channel) {
             this.playlist.push(args[0]);
 
@@ -25,9 +28,7 @@ class MusicModule {
                 return;
             }
 
-            if (!this.connection) {
-                this.connection = await channel.join();
-            }
+            await this._tryConnectChannel(message);
 
             this.isPlaying = true;
 
@@ -39,11 +40,39 @@ class MusicModule {
         await this._playNextMusic();
     }
 
+    stop(message, args) {
+        this.playlist = [];
+        this.isPlaying = false;
+        this.currentStream && this.currentStream.destroy();
+        this.connection && this.connection.disconnect();
+        clearInterval(this.radioInterval);
+        this.radioInterval = null;
+        this.connection = null;
+    }
+
+    /**
+     *
+     * @param {Message} message
+     * @param {*} args
+     */
+    async radio(message, args) {
+        const types = {
+            kpop: "https://listen.moe/kpop/opus",
+            jpop: "https://listen.moe/opus",
+        };
+        const url = types[args[0]];
+
+        if (url) {
+            await this._tryConnectChannel(message);
+            await this._playRadio(url);
+            this._setupRadioInterval(url);
+        }
+    }
+
     async debug(message, args) {
         console.log(this.playlist);
         console.log(this.currentMusic);
         console.log(this.currentStream);
-
     }
 
     async _playNextMusic() {
@@ -54,7 +83,7 @@ class MusicModule {
             .play(audioPipe, {
                 type: "opus",
                 volume: 0.05,
-                highWaterMark: 1<<25
+                highWaterMark: 1 << 25,
             })
             .on("finish", () => {
                 if (this.playlist.length) {
@@ -69,8 +98,56 @@ class MusicModule {
             });
     }
 
+    async _playRadio(url) {
+        this.isLoading = true;
+        const response = await axios({
+            method: "get",
+            url: url,
+            responseType: "stream",
+        });
+
+        mm.parseStream(response.data, { mimeType: "audio/ogg" }).then((metadata) => {
+            this.messageChannel.send(`Current Playing: ${metadata.common.title} by ${metadata.common.artist}`);
+        });
+
+        this.currentStream && this.currentStream.destroy();
+        this.currentStream = this.connection
+            .play(response.data, {
+                type: "ogg/opus",
+                volume: 0.05,
+                highWaterMark: 1 << 25,
+            })
+            .on("error", (e) => {
+                console.log(e);
+            })
+            .once("start", () => {
+                this.isLoading = false;
+            });
+    }
+
+    /**
+     *
+     * @param {Message} message
+     */
+    async _tryConnectChannel(message) {
+        const channel = message.member.voice.channel;
+        if (!this.connection) {
+            this.connection = await channel.join();
+            this.messageChannel = message.channel;
+        }
+    }
+
+    _setupRadioInterval(url) {
+        clearInterval(this.radioInterval);
+        this.radioInterval = setInterval(() => {
+            if (!this.currentStream.writableLength && !this.isLoading) {
+                this._playRadio(url);
+            }
+        }, 1000);
+    }
+
     _getCurrentMusicPipe() {
-        return ytdl(this.currentMusic, { filter: "audioonly", quality: "highestaudio", highWaterMark: 1<<25 });
+        return ytdl(this.currentMusic, { filter: "audioonly", quality: "highestaudio", highWaterMark: 1 << 25 });
     }
 
     _getCommands() {
